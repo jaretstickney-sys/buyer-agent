1dab053d708f71e61cae5b064f20b00711152788
jaretstickney-sys <jaret.stickney@gmail.com>
2025-12-17 15:59:39 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 CONFIG = {
-    "HUBSPOT_ACCESS_TOKEN": os.getenv("HUBSPOT_ACCESS_TOKEN", "pat-na2-94786176-2082-4c20-bc28-9aeef427e6e4"),
+    "HUBSPOT_ACCESS_TOKEN": os.getenv("HUBSPOT_ACCESS_TOKEN"),
     "source_property": "19 Castle Rd, Narragansett, RI",
     "buyer_profile": "empty_nester",
     "allowed_origins": ["https://gilded-seahorse-172ddf.netlify.app/"]  # Replace "*" with your Netlify URL in production
